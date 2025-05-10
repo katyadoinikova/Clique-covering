3f883480a0fb297190d0dd4d85d932685f40744a
Екатерина Дойникова <k.doinikova@yandex.ru>
2025-05-10 21:43:07 +0300

    corrections in symmethrics, added checker

--- main.py
+++ main.py
@@ -6,25 +6,24 @@ from pulp import LpProblem, LpVariable, LpMinimize, lpSum
 
 import networkx as nx
 
-def generate_triangle_free_graph(n):
-    total_nodes = 3 * n
-    G = nx.complete_graph(total_nodes)
+def check_solution(x):
+    for k in range(num_v):
+        for i in range(3*n):
+            for j in range(i + 1, 3*n):
+                if x[i][k].varValue == 1 and x[j][k].varValue == 1:
+                    assert (i // 3 != j // 3)
+    for i in range(3 * n):
+        for j in range(i + 1, 3 * n):
+            if i // 3 != j // 3:
+                is_covered = False
+                for k in range(num_v):
+                    if x[i][k].varValue == 1 and x[j][k].varValue == 1:
+                        is_covered = True
+                        break
+                assert is_covered
+    print("Checked")
 
-    vertices = list(range(total_nodes))
-
-    for i in range(n):
-        u, v, w = vertices[3 * i:3 * i + 3]
-        G.remove_edge(u, v)
-        G.remove_edge(v, w)
-        G.remove_edge(w, u)
-
-    return G
-
-
-
-
-n = 5
-graph = generate_triangle_free_graph(n)
+n = 6
 vertices = list(range(3*n))
 
 if n <= 2:
@@ -35,67 +34,49 @@ else:
 model = LpProblem("Clique_Covering", LpMinimize)
 
 vert = LpVariable.dicts("x", (vertices, range(num_v)), cat="Binary")
-y = LpVariable.dicts("y", range(num_v), cat="Binary")
 
 model += 0
-model += lpSum(y[k] for k in range(num_v)) == 8
+num_v = 13
 
 edges = LpVariable.dicts("z", (vertices, vertices, range(num_v)), cat="Binary")
 
-m = LpVariable.dicts("m", (range(n), range(num_v)), cat="Binary")
+m = LpVariable.dicts("m", (range(n), range(num_v)), cat="Binary") # m[i][j] = 1 - минимальное i, что vert[i][j] != 1
 
-s = LpVariable.dicts("s", (range(n), range(num_v)), cat="Binary")
 
 
 for i in range(0, len(vertices), 3):
     model += (vert[i][0] == 1)
 
-for i in range(0, n - 1):
-    for k in range(num_v):
-        model += s[i][k] >= vert[3 * i][k]
-        model += s[i][k] >= vert[3 * i + 1][k]
-        model += s[i][k] >= vert[3 * i + 2][k]
-        model += s[i][k] <= vert[3 * i][k] + vert[3 * i + 1][k] + vert[3 * i + 2][k]
-for i in range(n - 1):
-    lhs = lpSum(2**(num_v - 1 - t) * s[i][t] for t in range(num_v))
-    rhs = lpSum(2**(num_v - 1 - t) * s[i+1][t] for t in range(num_v))
-    model += lhs >= rhs
 
 for i in range(n):
     for j in range(num_v):
-        model += (m[n][j] <= 1 - vert[i*3][j])
+        model += (m[i][j] <= 1 - vert[i*3][j])
         for k in range(j):
-            model += (m[n][j] <= vert[i*3][k])
+            model += (m[i][j] <= vert[i*3][k])
 
 for i in vertices:
     for j in vertices:
         for k in range(num_v):
-            if i < j:
+            if i < j and i // 3 != j // 3:
                 model += edges[i][j][k] <= vert[i][k]
                 model += edges[i][j][k] <= vert[j][k]
-                model += (edges[i][j][k] >= vert[i][k] + vert[j][k] - 1)
+                model += edges[i][j][k] >= vert[i][k] + vert[j][k] - 1
 
-
-if len(vertices) > 5:
-    model += edges[0][3][0] == 1
-    model += edges[0][4][1] == 1
-    model += edges[0][5][2] == 1
+for k in range(num_v - 1):
+    for i in range(n):
+        model += m[i][k] >= m[i][k + 1]
 
 for k in range(num_v):
     for i in range(0, len(vertices), 3):
-        model += vert[i][k] + vert[i + 1][k] + vert[i + 2][k] <= 1
+        model += vert[i][k] + vert[i + 1][k] + vert[i + 2][k] == 1
 
 for k in range(num_v):
     for i in range(n):
         model += vert[i*3 + 2][k] <= 1 - m[i][k]
 
-for i in vertices:
-    for k in range(num_v):
-        model += vert[i][k] <= y[k]
-
-for i in vertices:
-    for j in graph[i]:
-        if i < j:
+for i in range(3 * n):
+    for j in range(i + 1, 3 * n):
+        if i // 3 != j // 3:
             model += lpSum(edges[i][j][k] for k in range(num_v)) >= 1
 
 
@@ -113,20 +94,19 @@ if model.status == 1:
     #             if x[i][k].varValue == 1 and x[j][k].varValue == 1 and i < j:
     #                 print(f"Edge {i}-{j} is covered by Clique {k + 1}.")
 
-    count = 0
-    for k in range(num_v):
-        if y[k].varValue == 1:
-            count+=1
-    print(count)
+
+
+    print(num_v)
+    check_solution(vert)
 
     # for k in range(num_v):
     #     for v in vertices:
-    #         print(f"x__{v}_{k} = {x[v][k].varValue}")
+    #         print(f"x__{v}_{k} = {vert[v][k].varValue}")
     # for k in range(7, 8):
     #     for i in vertices:
     #         for j in vertices:
     #             if i < j:
-    #                 print(x[i][k].varValue + x[j][k].varValue - 1)
+    #                 print(x[i][k].varValue + vert[j][k].varValue - 1)
     #                 print(f"z__{i}_{j}_{k} = {z[i][j][k].varValue}")
     #
     # for name, constraint in model.constraints.items():
@@ -135,7 +115,3 @@ if model.status == 1:
 
 else:
     print("No solution found.")
-
-
-# checker
-
